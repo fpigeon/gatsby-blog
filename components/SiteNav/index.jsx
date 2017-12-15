f3eb216a6733f3876bc68899d53f47ed5b95e0d1
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-12-15 13:39:13 -0600

    adds uses page that feautures the hw/sw I use every day

--- components/SiteNav/index.jsx
+++ components/SiteNav/index.jsx
@@ -1,38 +1,56 @@
-import React from 'react'
-import { RouteHandler, Link } from 'react-router'
-import { prefixLink } from 'gatsby-helpers'
-import './style.css'
+import React from "react";
+import { RouteHandler, Link } from "react-router";
+import { prefixLink } from "gatsby-helpers";
+import "./style.css";
 
 class SiteNav extends React.Component {
-    render() {
-        const {location} = this.props
-        return (
-            <nav className='blog-nav'>
-              <ul>
-                <li>
-                  <Link to={ prefixLink('/')} activeClassName="current" onlyActiveOnIndex> Articles
-                  </Link>
-                </li>
-                <li>
-                  <Link to={ prefixLink('/about/')} activeClassName="current"> About Me
-                  </Link>
-                </li>
-                <li>
-                  <Link to={ prefixLink('/resume/')} activeClassName="current"> Resume
-                  </Link>
-                </li>
-                <li>
-                  <Link to={ prefixLink('/portfolio/')} activeClassName="current"> Portfolio
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            );
-    }
+  render() {
+    const { location } = this.props;
+    return (
+      <nav className="blog-nav">
+        <ul>
+          <li>
+            <Link
+              to={prefixLink("/")}
+              activeClassName="current"
+              onlyActiveOnIndex
+            >
+              {" "}
+              Articles
+            </Link>
+          </li>
+          <li>
+            <Link to={prefixLink("/about/")} activeClassName="current">
+              {" "}
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link to={prefixLink("/resume/")} activeClassName="current">
+              {" "}
+              Resume
+            </Link>
+          </li>
+          <li>
+            <Link to={prefixLink("/portfolio/")} activeClassName="current">
+              {" "}
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link to={prefixLink("/uses/")} activeClassName="current">
+              {" "}
+              Uses
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 SiteNav.propTypes = {
-    location: React.PropTypes.object,
-}
+  location: React.PropTypes.object
+};
 
-export default SiteNav
+export default SiteNav;
