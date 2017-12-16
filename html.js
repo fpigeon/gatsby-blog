87c54415abb7da5da0b3b7ee6ff31fddaaee60a8
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-12-16 00:41:24 -0600

    adds discus blog comments. need to test in prodcution. starts new blog post Year In Review.

--- html.js
+++ html.js
@@ -1,38 +1,74 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { prefixLink } from 'gatsby-helpers'
+import React from "react";
+import Helmet from "react-helmet";
+import { prefixLink } from "gatsby-helpers";
 
-const BUILD_TIME = new Date().getTime()
+const BUILD_TIME = new Date().getTime();
 
 module.exports = React.createClass({
-    displayName: 'HTML',
-    propTypes: {
-        body: React.PropTypes.string,
-    },
-    render() {
-        const {body, route} = this.props
-        const {title} = Helmet.rewind()
-        const font = <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
-        let css
-        if (process.env.NODE_ENV === 'production') {
-            css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
-        }
+  displayName: "HTML",
+  propTypes: {
+    body: React.PropTypes.string
+  },
+  render() {
+    const { body, route } = this.props;
+    const { title } = Helmet.rewind();
+    const font = (
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700&subset=latin,cyrillic"
+        rel="stylesheet"
+        type="text/css"
+      />
+    );
+    let css;
+    if (process.env.NODE_ENV === "production") {
+      css = (
+        <style
+          dangerouslySetInnerHTML={{
+            __html: require("!raw!./public/styles.css")
+          }}
+        />
+      );
+    }
 
-        return (
-            <html lang="en">
-            <head>
-              <meta charSet="utf-8" />
-              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
-              { title.toComponent() }
-              { font }
-              { css }
-            </head>
-            <body>
-              <div id="react-mount" dangerouslySetInnerHTML={ {    __html: this.props.body} } />
-              <script src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
-            </body>
-            </html>
-        )
-    },
-})
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0 maximum-scale=5.0"
+          />
+          {title.toComponent()}
+          {font}
+          {css}
+        </head>
+        <body>
+          <div
+            id="react-mount"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
+          <script>
+            /**
+            *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+            *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+            /*
+            var disqus_config = function () {
+            this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            };
+            */
+            (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://http-frankpigeon-com.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+            })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        </body>
+      </html>
+    );
+  }
+});
---
+++ pages/articles/2017-12-15-year-in-review/index.md
@@ -0,0 +1,32 @@
+---
+title: "2017: Year in Review"
+date: "2017-12-15"
+layout: post
+path: "/year-in-review/"
+category: "reviews"
+description: "The year that went by too fast"
+readNext: "/goals-for-2017"
+---
+
+![fireworks](./fireworks.jpeg)
+
+## Looking Back
+
+It's hard to believe that Christmas is just a week away and we are almost done with 2017. This time of year, at least for me, is a time of reflection on the year gone by. It seems like
+
+## Challenges and Opportunities
+
+* selecting a js and frameworks
+* freelancing
+* react in production
+* interview
+
+## Best Upgrades
+
+* standing desk
+* exercise as part of a routine
+* qhd monitor
+
+## What's in Store for 2018
+
+* react nano degree
